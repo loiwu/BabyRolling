5c3ac4cb22c5f776c7f95046872e7c296915e015
LoiWu <superloiwu@gmail.com>
2015-02-07 14:47:31 +0800

    - (void)didLoadFromCCB
    - (void)didLoadFromCCB
    {
        NSLog(@"GameScene created");
    }

---
+++ BabeRolling.spritebuilder/Source/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  BabeRolling
+//
+//  Created by Loi Wu on 2/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface GameScene : CCNode
+
+@end
---
+++ BabeRolling.spritebuilder/Source/GameScene.m
@@ -0,0 +1,18 @@
+//
+//  GameScene.m
+//  BabeRolling
+//
+//  Created by Loi Wu on 2/7/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "GameScene.h"
+
+@implementation GameScene
+
+- (void)didLoadFromCCB
+{
+    NSLog(@"GameScene created");
+}
+
+@end
